068062ea4781c2082bad89c37f9e72926accaf06
mitchelldirt <mitchellmudd@outlook.com>
2022-08-30 15:30:45 -0400

    Updated with content that was in stackblitz

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
+function MyButton() {
+  return (<button className='testClass'>Hello</button>);
+}
 
+/* 
 export default function App() {
   return (
-    <>
+    <div>
+      <MyButton />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
+*/
+
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+export default function Profile() {
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
   );
 }
