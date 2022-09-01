47ee6d8a941ab71bf6e3f7c76be5310874417182
mitchelldirt <mitchellmudd@outlook.com>
2022-09-01 12:18:02 -0400

    Learned some more about React like props. Practiced making more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
 
+// Create first component and add a class to it.
 function MyButton() {
   return (<button className='testClass'>Hello</button>);
 }
 
-/* 
-export default function App() {
+function Button() {
   return (
     <div>
       <MyButton />
@@ -15,15 +15,16 @@ export default function App() {
     </div>
   );
 }
-*/
 
+
+// How to use data in a component
 const user = {
   name: 'Hedy Lamarr',
   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
   imageSize: 90,
 };
 
-export default function Profile() {
+function UserAvatar({ user }) {
   return (
     <>
       <h1>{user.name}</h1>
@@ -36,6 +37,51 @@ export default function Profile() {
           height: user.imageSize
         }}
       />
+    </>
+  )
+}
+
+
+
+function AdminPanel() {
+  return (<div>
+    <p>Admin Panel</p>
+  </div>)
+}
+
+function LoginForm() {
+  return (<p>Please login!</p>)
+}
+let content;
+let isLoggedIn = false;
+function DisplayAdminOrLogin() {
+  if (isLoggedIn) {
+    content = <AdminPanel />
+  } else {
+    content = <LoginForm />
+  }
+  return (
+    <div>
+      {content}
+    </div>
+  )
+}
+
+
+
+export default function Profile() {
+  return (
+    <>
+      {/* Putting all the logic into a component */}
+      <DisplayAdminOrLogin />
+      { /*Short circuiting to get the correct component */}
+      <div>
+        {isLoggedIn && <AdminPanel />}
+        {!isLoggedIn && <LoginForm />}
+      </div>
+
+      {/* Using stateless pros to create a UserAvatar component */}
+      <UserAvatar user={user} />
     </>
   );
 }
